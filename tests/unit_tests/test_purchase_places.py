d0aca1301bbf20efe4e735c7d0f19a9fe55b80dd
Grégory Lama <gregory.lama@gmail.com>
2024-02-21 00:33:53 +0100

    fix(Gudlft): clubs should not be able to book more than 12 places per competition

--- tests/unit_tests/test_purchase_places.py
+++ tests/unit_tests/test_purchase_places.py
@@ -22,15 +22,26 @@ class TestPurchasePlaces:
         {
             "name": "Test club",
             "email": "test_club@example.com",
-            "points": "10",
+            "points": "15",
+        }
+    ]
+
+    booked_places = [
+        {
+            "competition": "Test compétition",
+            "club": "Test club",
+            "places": 0,
         }
     ]
 
     def setup_method(self):
         server.competitions = self.competitions
         server.clubs = self.clubs
-        # Reset the number of places
+        server.booked_places = self.booked_places
+        # Reset initial values
         self.competitions[0]["numberOfPlaces"] = 25
+        self.clubs[0]["points"] = 15
+        self.booked_places[0]["places"] = 0
 
     def test_purchase_places_with_less_places_than_available(self, client):
         places_booked = 30
@@ -42,7 +53,7 @@ class TestPurchasePlaces:
         assert response.status_code == 400
         assert data.find(b"You do not have enough points") != -1
         assert int(self.competitions[0]["numberOfPlaces"]) == 25
-        assert int(self.clubs[0]["points"]) == 10
+        assert int(self.clubs[0]["points"]) == 15
 
     def test_purchase_places(self, client):
         places_booked = 5
@@ -54,4 +65,15 @@ class TestPurchasePlaces:
         assert response.status_code == 200
         assert data.find(b"Great-booking complete!") != -1
         assert self.competitions[0]["numberOfPlaces"] == 20
-        assert self.clubs[0]["points"] == 5
+        assert self.clubs[0]["points"] == 10
+
+    def test_should_not_be_able_to_purchase_more_than_12_places(self, client):
+        places_booked = 14
+        response = client.post(
+            '/purchasePlaces',
+            data=dict(competition=self.competitions[0]["name"], club=self.clubs[0]["name"], places=places_booked),
+        )
+        data = response.get_data()
+        assert response.status_code == 400
+        assert data.find(b"Sorry, you cannot purchase more than 12 places") != -1
+        assert self.booked_places[0]["places"] == 0
