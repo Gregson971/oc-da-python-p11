6210823ca2fdcf0f631b52f4bf351e13f9369026
Grégory Lama <gregory.lama@gmail.com>
2024-02-21 02:47:28 +0100

    fix(Gudlft): booking places in past competitions

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import json
 from flask import Flask, render_template, request, redirect, flash, url_for
+from datetime import datetime
+from dateutil.parser import parse as parse_date
 
 
 def loadClubs():
@@ -14,11 +16,25 @@ def loadCompetitions():
         return listOfCompetitions
 
 
+def sort_competitions_date(comps):
+    past = []
+    present = []
+
+    for comp in comps:
+        if parse_date(comp['date']) < datetime.now():
+            past.append(comp)
+        elif parse_date(comp['date']) >= datetime.now():
+            present.append(comp)
+
+    return past, present
+
+
 app = Flask(__name__)
 app.secret_key = 'something_special'
 
 competitions = loadCompetitions()
 clubs = loadClubs()
+past_competitions, present_competitions = sort_competitions_date(competitions)
 
 
 @app.route('/')
@@ -29,28 +45,54 @@ def index():
 @app.route('/showSummary', methods=['POST'])
 def showSummary():
     club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html', club=club, competitions=competitions)
+    return render_template(
+        'welcome.html', club=club, past_competitions=past_competitions, present_competitions=present_competitions
+    )
 
 
 @app.route('/book/<competition>/<club>')
 def book(competition, club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template('booking.html', club=foundClub, competition=foundCompetition)
+    found_club = [c for c in clubs if c['name'] == club][0]
+    found_competition = [c for c in competitions if c['name'] == competition][0]
+    if found_club and found_competition:
+        if parse_date(found_competition['date']) < datetime.now():
+            flash("This competition is over.", 'error')
+            return (
+                render_template(
+                    'welcome.html',
+                    club=club,
+                    past_competitions=past_competitions,
+                    present_competitions=present_competitions,
+                ),
+                400,
+            )
+        return render_template('booking.html', club=found_club, competition=found_competition)
     else:
-        flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        flash("Something went wrong-please try again", 'error')
+        return (
+            render_template(
+                'welcome.html',
+                club=club,
+                past_competitions=past_competitions,
+                present_competitions=present_competitions,
+            ),
+            400,
+        )
 
 
 @app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
-    flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    places_required = int(request.form['places'])
+    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
+    flash('Great-booking complete!', 'success')
+    return render_template(
+        'welcome.html',
+        club=club,
+        past_competitions=past_competitions,
+        present_competitions=present_competitions,
+    )
 
 
 # TODO: Add route for points display
---
+++ tests/unit_tests/test_book_places.py
@@ -0,0 +1,48 @@
+import pytest
+import server
+from server import app
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+class TestBookPlaces:
+    competitions = [
+        {
+            "name": "Test compétition",
+            "date": "2024-10-19 11:30:00",
+            "numberOfPlaces": "25",
+        },
+        {
+            "name": "Test 2 compétition",
+            "date": "2023-10-19 11:30:00",
+            "numberOfPlaces": "25",
+        },
+    ]
+
+    clubs = [
+        {
+            "name": "Test club",
+            "email": "test_club@example.com",
+            "points": "15",
+        }
+    ]
+
+    def setup_method(self):
+        server.competitions = self.competitions
+        server.clubs = self.clubs
+
+    def test_should_not_book_places_in_past_competition(self, client):
+        response = client.get("/book/Test 2 compétition/Test club")
+        assert response.status_code == 400
+        data = response.data.decode()
+        assert data.find("This competition is over.") != -1
+
+    def test_should_book_places(self, client):
+        response = client.get("/book/Test compétition/Test club")
+        assert response.status_code == 200
+        data = response.data.decode()
+        assert data.find("Booking for Test compétition || GUDLFT") != -1
